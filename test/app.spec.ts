95faff074b040b67cf4aeef0f795a36ccde1f1c2
Benny Halperin <bhalperin@sealights.io>
2023-09-04 12:09:13 +0300

    Fixed the single unit test

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -3,8 +3,10 @@ import { App } from '../src/app';
 
 describe('my-app', () => {
 	it('should render message', async () => {
-		const node = (await render('<app></app>', App)).firstElementChild;
-		const text = node.textContent;
-		expect(text.trim()).toBe('Hello World!');
+		const node = (await render('<app></app>', App)).querySelector('nav');
+		const navLinks = node.children;
+
+		expect(navLinks.length).toBe(3);
+		expect(navLinks[0].textContent.toLowerCase()).toBe('about');
 	});
 });
