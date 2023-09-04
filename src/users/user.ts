5861d204b17c7f00d647fb778653c322e060ff55
Benny Halperin <bhalperin@sealights.io>
2023-09-04 12:10:52 +0300

    Implemented BS tooltip instead of browser built-in tooltip

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,28 +1,50 @@
 import { bindable, EventAggregator, inject } from 'aurelia';
+import * as bootstrap from 'bootstrap';
 import { Rest } from '../util/rest';
 import { UserData, UserListItemData } from './users-data';
 
 @inject(EventAggregator, Rest)
 export class User {
 	@bindable public userListItem: UserListItemData;
+	readonly #FLIPPED_CLASS: string = 'is-flipped';
+	#isUserRetrieved = false;
 	user: UserData;
-	private cardPanel: Element;
-	private isUserRetrieved = false;
-	private readonly FLIPPED_CLASS: string = 'is-flipped';
+	private cardPanel: HTMLElement;
 
 	constructor(private ea: EventAggregator, private rest: Rest) { }
+
+	async #getUser(): Promise<void> {
+		if (this.#isUserRetrieved) {
+			return;
+		}
+
+		this.user = await this.rest.getUser(this.userListItem.login);
+		this.#isUserRetrieved = true;
+	}
+
+	#flipUser(): void {
+		this.cardPanel?.classList.toggle(this.#FLIPPED_CLASS);
+		this.#enableTooltip();
+	}
+
+	#enableTooltip(): void {
+		const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+
+		tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+	}
 
 	public created() {
 		this.subscribe();
 	}
 
 	public attached() {
-		this.isUserRetrieved = false;
+		this.#isUserRetrieved = false;
+		this.#enableTooltip();
 	}
 
 	public subscribe(): void {
 		this.ea.subscribe('flipToFront', () => {
-			this.cardPanel?.classList.remove(this.FLIPPED_CLASS);
+			this.cardPanel?.classList.remove(this.#FLIPPED_CLASS);
 		});
 	}
 
@@ -30,18 +52,13 @@ export class User {
 		this.ea.publish('userSelected', user);
 	}
 
-	public async getUser(userLogin: string): Promise<void> {
-		this.flipUser();
+	public async flipClicked(ev: MouseEvent, frontClicked: boolean): Promise<void> {
+		bootstrap.Tooltip.getInstance(ev.target as HTMLElement).hide();
 
-		if (this.isUserRetrieved) {
-			return;
+		if (frontClicked) {
+			await this.#getUser();
 		}
 
-		this.user = await this.rest.getUser(userLogin);
-		this.isUserRetrieved = true;
-	}
-
-	public flipUser(): void {
-		this.cardPanel?.classList.toggle(this.FLIPPED_CLASS);
+		this.#flipUser();
 	}
 }
